9b3c1e81af8e5c20b91abc1de14c74fcdaa2cd75
Oriol Flors Mas <oriol.flors.mas@gmail.com>
2020-12-26 19:55:50 +0100

    Refactored problem 23

--- 23/main.go
+++ 23/main.go
@@ -12,30 +12,38 @@ type cup struct {
 }
 
 func main() {
-	//	util.Solve(solve1, solve2) // naive
-	util.Solve(solveQuick1, solveQuick2)
+	util.Solve(solve1, solve2)
 }
 
-func solveQuick1(lines []string) interface{} {
-	circle, valuesMap := parseLinked(parseInput(lines))
-	for i := 0; i < 100; i++ {
-		circle = stepLinked(circle, valuesMap, 9)
-	}
+func solve1(lines []string) interface{} {
+	circle := step(parseInput(lines), 100, 9)
 	return formatLinked(circle)
 }
 
-func solveQuick2(lines []string) interface{} {
-	circle, valuesMap := parseLinked(completeCircle(parseInput(lines)))
-	for i := 0; i < 10000000; i++ {
-		circle = stepLinked(circle, valuesMap, 1000000)
-	}
-
-	one := valuesMap[1]
+func solve2(lines []string) interface{} {
+	one := step(completeCircle(parseInput(lines)), 10000000, 1000000)
 	return one.next.value * one.next.next.value
 }
 
-func parseLinked(l []int) (*cup, map[int]*cup) {
-	m := make(map[int]*cup)
+func parseInput(l []string) []int { return util.ParseInts(strings.Split(l[0], "")) }
+
+func completeCircle(in []int) []int {
+	for i := 10; i <= 1000000; i++ {
+		in = append(in, i)
+	}
+	return in
+}
+
+func step(nums []int, n, m int) *cup {
+	circle, valuesMap := parseLinked(nums)
+	for i := 0; i < n; i++ {
+		circle = stepLinked(circle, valuesMap, m)
+	}
+	return valuesMap[1]
+}
+
+func parseLinked(l []int) (*cup, []*cup) {
+	m := make([]*cup, len(l)+1)
 	originalHead := &cup{value: l[0]}
 	m[l[0]] = originalHead
 	head := originalHead
@@ -50,171 +58,36 @@ func parseLinked(l []int) (*cup, map[int]*cup) {
 	return originalHead, m
 }
 
-func stepLinked(circle *cup, valuesMap map[int]*cup, max int) *cup {
+func stepLinked(circle *cup, valuesMap []*cup, max int) *cup {
 	current := circle.value
-	pickedUp, circle := pickUpLinked(circle)
-	destination := getDestinationLinked(current, max, pickedUp, circle)
-	insertLinked(circle, pickedUp, valuesMap[destination])
+	pickedUp, circle := pickUp(circle)
+	destination := getDestination(current, max, pickedUp, circle)
+	insert(circle, pickedUp, valuesMap[destination])
 	return circle
 }
 
-func solve1(lines []string) interface{} {
-	circle, index := parseInput(lines), 0
-	for i := 0; i < 100; i++ {
-		circle, index = step(circle, index)
-	}
-	return formatCircle(circle)
-}
-
-func solve2(lines []string) interface{} {
-	circle, index := parseInput(lines), 0
-	circle = completeCircle(circle)
-	for i := 0; i < 10000000; i++ {
-		if i%1000 == 0 {
-			fmt.Println(i / 1000)
-		}
-		circle, index = step(circle, index)
-	}
-	index = util.IntIndexOf(1, circle)
-	return circle[index+1] + circle[index+2]
-}
-
-func print(args ...interface{}) {
-	fmt.Println(args...)
-}
-
-func parseInput(lines []string) (out []int) {
-	l := strings.Split(lines[0], "")
-	for _, x := range l {
-		out = append(out, util.ParseInt(x))
-	}
-	return out
-}
-
-func completeCircle(in []int) (out []int) {
-	for _, x := range in {
-		out = append(out, x)
-	}
-	for i := 10; i <= 1000000; i++ {
-		out = append(out, i)
-	}
-	return out
-}
-
-func formatCircle(in []int) (out string) {
-	index := util.IntIndexOf(1, in)
-	for i := index; i < len(in); i++ {
-		out += fmt.Sprintf("%d", in[i])
-	}
-	for i := 0; i < index; i++ {
-		out += fmt.Sprintf("%d", in[i])
-	}
-	return out[1:]
-}
-
-func step(in []int, index int) (out []int, outindex int) {
-	current := in[index]
-	next := in[(index+4)%len(in)]
-	remaining, pickedUp := pickUp(in, index)
-	destination := getDestination(current, len(in), pickedUp)
-	out = insert(remaining, destination, pickedUp)
-	//	fmt.Println(in, remaining, pickedUp, destination, out)
-	return out, util.IntIndexOf(next, out)
-}
-
-func pickUp(in []int, index int) (out, picked []int) {
-	index++
-	if index >= len(in) {
-		index = 0
-	}
-	if index+2 < len(in) {
-		for i := 0; i < index; i++ {
-			out = append(out, in[i])
-		}
-		for i := index; i < index+3; i++ {
-			picked = append(picked, in[i])
-		}
-		for i := index + 3; i < len(in); i++ {
-			out = append(out, in[i])
-		}
-	} else {
-		for i := index; i < index+3; i++ {
-			picked = append(picked, in[i%len(in)])
-		}
-		for i := (index + 3) % len(in); i < index; i++ {
-			out = append(out, in[i])
-		}
-	}
-
-	return out, picked
-}
-
-func getDestination(current, max int, in []int) int {
-	for {
-		current--
-		if current == 0 {
-			current = max
-		}
-		if !util.IntInSlice(current, in) {
-			return current
-		}
-	}
-}
-
-func getDestinationLinked(current, max int, pickedUp, circle *cup) int {
+func getDestination(current, max int, pickedUp, circle *cup) int {
 LOOP:
 	for {
 		current--
 		if current == 0 {
 			current = max
 		}
-		tmp := pickedUp
-		for i := 0; i < 3; i++ {
-			if tmp.value == current {
-				continue LOOP
-			}
-			tmp = tmp.next
+		if pickedUp.value == current ||
+			pickedUp.next.value == current ||
+			pickedUp.next.next.value == current {
+			continue LOOP
 		}
 		return current
 	}
 }
 
-func insertLinked(circle, pickedUp, destination *cup) {
-	next := destination.next
+func insert(circle, pickedUp, destination *cup) {
+	pickedUp.next.next.next = destination.next
 	destination.next = pickedUp
-	for i := 0; i < 2; i++ {
-		pickedUp = pickedUp.next
-	}
-	pickedUp.next = next
-}
-
-func insert(in []int, destination int, picked []int) (out []int) {
-	index := util.IntIndexOf(destination, in) + 1
-	for i := 0; i < index; i++ {
-		out = append(out, in[i])
-	}
-	for _, x := range picked {
-		out = append(out, x)
-	}
-	for i := index; i < len(in); i++ {
-		out = append(out, in[i])
-	}
-	return out
-}
-
-func printLinked(circle *cup, max int) {
-	head := circle
-	for i := 0; i < max; i++ {
-		fmt.Printf("%d ", head.value)
-		head = head.next
-	}
-	fmt.Println()
 }
 
 func formatLinked(circle *cup) (out string) {
-	for circle.value != 1 {
-		circle = circle.next
-	}
 	circle = circle.next
 	for circle.value != 1 {
 		out += fmt.Sprintf("%d", circle.value)
@@ -223,13 +96,9 @@ func formatLinked(circle *cup) (out string) {
 	return out
 }
 
-func pickUpLinked(circle *cup) (picked *cup, out *cup) {
-	head := circle
+func pickUp(circle *cup) (picked *cup, out *cup) {
 	picked = circle.next
-	fourth := circle
-	for i := 0; i < 4; i++ {
-		fourth = fourth.next
-	}
-	head.next = fourth
+	fourth := circle.next.next.next.next
+	circle.next = fourth
 	return picked, fourth
 }
